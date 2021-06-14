cabbb79d062ba4b82441508084d28d0832a6753b
Charles Severance <csev@umich.edu>
2021-06-13 23:57:30 -0400

    Change is good.

--- index2.php
+++ index2.php
@@ -1,6 +1,19 @@
 <h1>Hello</h1>
+<a href="index2.php">Click</a>
     <script>
-        navigator.serviceWorker.register('sw.js')
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/zap/sw.js').then(function(registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+console.log(registration);
+    }, function(err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
+
     </script>
 <hr>
 <pre>
--- sw.js
+++ sw.js
@@ -1,12 +1,43 @@
-self.addEventListener('fetch', function(event) {
-    console.log('Yada');
-    const newRequest = new Request(event.request, {
-        headers: {"Authorization": "Bearer XXX-my-token"},
-        mode: "cors"
-    });
-    console.log('Yada 1');
-    return fetch(newRequest);
+self.addEventListener('install', function(event) {
+  console.log("Perform install steps");
 });
+
+self.addEventListener('activate', function(event) {
+  console.log("Activated");
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(customHeaderRequestFetch(event))
+})
+
+function customHeaderRequestFetch(event) {
+  // decide for yourself which values you provide to mode and credentials
+  const newRequest = new Request(event.request, {
+    mode: 'cors',
+    credentials: 'omit',
+    headers: {
+      'x-my-custom-header': 'The Most Amazing Header Ever'
+    }
+  })
+  console.log('Returning 2.');
+    newRequest.headers['X-Chuck'] = "42";
+  console.log(newRequest);
+  return fetch(newRequest)
+}
+
+/*
+
+self.addEventListener('fetch', function(event) {
+    console.log('Yada url ', event.request.url);
+    console.log(self);
+    console.log('Event');
+    console.log(event);
+    event.request.headers['X-Chuck'] = "42";
+    console.log('Yada 1');
+    console.log(event);
+    return;
+});
+*/
 
 console.log('Zap');
 
